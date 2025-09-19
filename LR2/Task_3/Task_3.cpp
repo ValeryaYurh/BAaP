f2aa1288bf029c63bdef5d94c8d994b4350cb3c6
Юргилевич Валерия <lerayurhilevich@icloud.com>
2025-09-19 13:54:11 +0300

    LR2 Task_3

--- LR2/Task_3/Task_3.cpp
+++ LR2/Task_3/Task_3.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    
+   double b1, n, q, S;
+   cout << "Ввести b1: ";
+   cin >> b1;
+   cout << "Ввести n: ";
+   cin >> n;
+   q = 1/(n + 1);
+   S = b1/(1 - q);
+   cout << "Cумма всех членов убывающей геометрической прогрессии = " << S;
+return 0; 
 }
