3a1e11b92e0e3095dbd473369c664ee5b8c4bd07
Юргилевич Валерия <lerayurhilevich@icloud.com>
2025-09-21 00:45:50 +0300

    task7 готов

--- LR2/Task_6/Task_6.cpp
+++ LR2/Task_6/Task_6.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main(){
-  int A, Y, C, x, y;
-  double D, K, S;
-  cout << "Ввести x, y, C, K: ";
-  cin >> x >> y >> C >> K;
-  A = x + y;
-  D = abs(C-A);
-  S = 10.1 + (A/C) + D/pow(K, 2);
-  cout << "S = " << S;
+  int h;
+  double x, p, A, B, C, D, K, Y;
+  cout << "Ввести x, p, h, K, C, D: ";
+  cin >> x >> p >> h >> K >> C >> D;
+  A = x - p;
+  B = log(h);
+  Y = 0.78 * B + pow(A, 3)/(K*C*D);
+  cout << "Y = " << Y;
   return 0;
 }
   
