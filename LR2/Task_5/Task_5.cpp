fbd31e39824ebf2dcb8abbdb0ece92f5d3c2ec31
Юргилевич Валерия <lerayurhilevich@icloud.com>
2025-09-21 11:40:11 +0300

    ветка main

--- LR2/Task_5/Task_5.cpp
+++ LR2/Task_5/Task_5.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main(){
     double X, Y;
-    
+    cout << "Ввести X, Y: ";
+    cin >> X >> Y;
+    bool condition = X>Y;
+    condition ?  cout << "Х - максималльное значение" : cout << "Y - максимальное значение";
     return 0;
 }
