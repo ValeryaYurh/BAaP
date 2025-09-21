a81a230a8ed1202bae327987ac5af1080abcf5e9
Юргилевич Валерия <lerayurhilevich@icloud.com>
2025-09-21 11:36:54 +0300

    ветка control

--- LR2/Task_5/Task_5.cpp
+++ LR2/Task_5/Task_5.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main(){
     double X, Y;
-    
+    cout << "Ввести X, Y: ";
+    cin >> X >> Y;
+    if(X>Y){ 
+        cout << "Х - максималльное значение"; 
+    }else{
+      cout << "Y - максимальное значение";
+    }
     return 0;
 }
