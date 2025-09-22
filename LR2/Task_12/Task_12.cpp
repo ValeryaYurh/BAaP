06f8345d6276fcbfd4a8e2b7472cece5795059f9
Юргилевич Валерия <lerayurhilevich@icloud.com>
2025-09-22 21:08:56 +0300

    LR2 Task_12

--- LR2/Task_12/Task_12.cpp
+++ LR2/Task_12/Task_12.cpp
@@ -1,34 +1,22 @@
 #include <iostream>
 
 int main() {
-    double a, b, c;
-    std::cout << "Enter a b c: ";
-    std::cin >> a >> b >> c;
+    double p, q;
+    std::cout << "Enter p q: ";
+    std::cin >> p >> q;
 
-    double A = 1;
-    double B = b / a;
-    double C = c / a - 2;
+    double eps = 1e-3;
+    bool found = false;
 
-    double D = B*B - 4*A*C;
-
-    if (D < 0) {
-        std::cout << "No real solutions\n";
-        return 0;
+    for (double x = -100; x <= 100; x += 0.001) {
+        double val = x*x*x + p*x + q;
+        if (val > -eps && val < eps) {
+            std::cout << "x = " << x << "\n";
+            found = true;
+        }
     }
 
-    double y1 = (-B + sqrt(D)) / (2*A);
-    double y2 = (-B - sqrt(D)) / (2*A);
-
-    for (int i = 0; i < 2; ++i) {
-        double y = (i == 0) ? y1 : y2;
-        double disc = y*y - 4;
-        if (disc < 0) continue;
-
-        double x1 = (y + sqrt(disc)) / 2;
-        double x2 = (y - sqrt(disc)) / 2;
-
-        std::cout << "x = " << x1 << " and x = " << x2 << "\n";
-    }
+    if (!found) std::cout << "No real solutions found in [-100,100]\n";
 
     return 0;
 }
