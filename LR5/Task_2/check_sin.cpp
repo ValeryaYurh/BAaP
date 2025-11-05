d54d72b62292570bbdab970ec2c5f5df4c79277d
Юргилевич Валерия <lerayurhilevich@icloud.com>
2025-11-05 23:55:42 +0300

    changes 2

--- LR5/Task_2/check_sin.cpp
+++ LR5/Task_2/check_sin.cpp
@@ -1,16 +1,17 @@
 #include "check_sin.h"
 #include <cmath> 
+using namespace std;
 
 const double EPS = 1e-10;
 
 bool any_sin_positive(double* A, int N)
 {
 
-    if (A == nullptr || N <= 0)
+    if (A == nullptr or N <= 0)
         return false;
 
     if (N == 1)
-        return std::sin(A[0]) > EPS;
+        return sin(A[0]) > EPS;
 
     int mid = N / 2;
 
